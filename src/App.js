ba173f6de500e666de82eeb354407ba971e47492
rajpranit <rajpranit1207@gmail.com>
2022-10-19 13:47:18 +0530

    Header and Nav Links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import { Route } from "react-router-dom";
 
 import Welcome from "./pages/welcome";
+import MainHeader from "./components/MainHeader";
 import Product from "./pages/products";
 
 function App() {
   return (
     <>
-      <div>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-      </div>
+      <MainHeader />
+      <main>
+        <div>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products">
+            <Product />
+          </Route>
+        </div>
+      </main>
     </>
   );
 }
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,5 +1,5 @@
 const Product = () => {
-  return <h1>This is the Product page</h1>;
+  return <h1>This is the Products page</h1>;
 };
 
 export default Product;
